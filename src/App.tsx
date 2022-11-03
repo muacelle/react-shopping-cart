15001b7aae895ff155a01b5dbc174a3267c78e8e
Marcelle Pernet <mevpernet@gmail.com>
2022-11-03 18:49:16 -0300

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import { Drawer } from '@mui/material'
+import LinearProgress from '@mui/material/LinearProgress'
+ import Grid from '@mui/material/Grid'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import Badge from '@mui/material/Badge'
+import { Wrapper } from './App.styles'
+
+// Types
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  const results = await fetch('https://fakestoreapi.com/products')
+  const products = await results.json()
+  return products
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+  console.log(data)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>App</h1>
     </div>
   )
 }
